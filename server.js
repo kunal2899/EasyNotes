5306744e03c7d3592139545cd5a9167160d62341
kunal2899 <kunaljain649@gmail.com>
2021-06-19 19:47:09 +0530

    ..

--- server.js
+++ server.js
@@ -12,4 +12,4 @@ res.sendFile(path.join(__dirname+'/dist/EasyNotes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8000);
+app.listen(process.env.PORT || 8080);
