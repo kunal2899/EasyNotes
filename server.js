b2f62e3ce8eb845e5c8c6b572fde9bc4a92dae24
kunal2899 <kunaljain649@gmail.com>
2021-06-19 22:48:07 +0530

    ..

--- server.js
+++ server.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const path = require('path');
+var cors = require('cors')
 
 const app = express();
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/EasyNotes'));
 
+app.use(cors());
+
 app.get('/*', function(req,res) {
     
 res.sendFile(path.join(__dirname+'/dist/EasyNotes/index.html'));
 });
-
-app.use(cors())
 
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
